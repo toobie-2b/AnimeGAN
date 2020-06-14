e8d037b096e56ac2b217e600bcfd94ae8add7d38
Kartikeya Shorya <kartikeyashorya@gmail.com>
2020-06-14 13:36:23 +0530

    Added model_utils.py

---
+++ model_utils.py
@@ -0,0 +1,62 @@
+import torch
+import torch.nn as nn
+
+
+def apply_weights(model):
+    classname = model.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(model.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(model.weight.data, 1.0, 0.02)
+        nn.init.constant_(model.bias.data, 0)
+
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+
+        self.model = nn.Sequential(
+                nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1),
+                nn.BatchNorm2d(64),
+                nn.LeakyReLU(),
+                nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
+                nn.BatchNorm2d(128),
+                nn.LeakyReLU(),
+                nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),
+                nn.BatchNorm2d(256),
+                nn.LeakyReLU(),
+                nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1),
+                nn.BatchNorm2d(512),
+                nn.LeakyReLU(),
+                nn.Conv2d(512, 1, kernel_size=4, stride=2, padding=0),
+                nn.Sigmoid()
+            )
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+        
+        self.model = nn.Sequential(
+            nn.ConvTranspose2d(100, 1024, kernel_size=4, stride=1, padding=0),
+            nn.BatchNorm2d(1024),
+            nn.LeakyReLU(),
+            nn.ConvTranspose2d(1024, 512, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(),
+            nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(),
+            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(),
+            nn.ConvTranspose2d(128, 3, kernel_size=4, stride=2, padding=1),
+            nn.Tanh()
+            )
+
+    def forward(self, x):
+        return self.model(x)
+        
