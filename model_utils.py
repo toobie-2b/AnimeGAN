ed19e38b9489e5a96caca438dfb449852bba8393
Kartikeya Shorya <kartikeyashorya@gmail.com>
2020-10-09 01:53:04 +0530

    Changed the slope of Leaky ReLU to -0.2

--- model_utils.py
+++ model_utils.py
@@ -18,16 +18,16 @@ class Discriminator(nn.Module):
         self.model = nn.Sequential(
                 nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1),
                 nn.BatchNorm2d(64),
-                nn.LeakyReLU(),
+                nn.LeakyReLU(0.2),
                 nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
                 nn.BatchNorm2d(128),
-                nn.LeakyReLU(),
+                nn.LeakyReLU(0.2),
                 nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),
                 nn.BatchNorm2d(256),
-                nn.LeakyReLU(),
+                nn.LeakyReLU(0.2),
                 nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1),
                 nn.BatchNorm2d(512),
-                nn.LeakyReLU(),
+                nn.LeakyReLU(0.2),
                 nn.Conv2d(512, 1, kernel_size=4, stride=2, padding=0),
                 nn.Sigmoid()
             )
